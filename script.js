a5cdfde64ea8134cab542f4952d97222059bea11
Ivanna Tchinda <91744423+ivanna-tchinda@users.noreply.github.com>
2022-05-27 16:17:37 +0200

    augmentation du score
    augmentation du score quand on tire un dé, et le score revient à 0 quand on tombe sur le dé 1

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
 const dice = document.getElementById('roll');
+let pp1 = document.getElementById('pointsPlayer1');
+let pp2 = document.getElementById('pointsPlayer2');
+
 let score1 = 0;
+let score2 = 0;
+
+
 
 
 
 
 function diceGame(){
     //le dé change de face à chaque click sur roll
-    var randomDice = Math.floor((Math.random()*6) + 1);
+    let randomDice = Math.floor((Math.random()*6) + 1);
     var diceLocation = "D" + randomDice + ".png";
     var display = document.querySelectorAll("img")[0];
     display.setAttribute("src", diceLocation);
 
-    //on ajoute les points aux deux joueurs
-    score1 =+ randomDice;
-    document.getElementById('pointsPlayer1').innerHTML =+ score1;
-    return score1;
+    //le joueur 1 joue
+
+    if(randomDice == 1){
+        score1 = 0;
+        pp1.innerHTML = score1;
+    }
+    else
+    {
+        score1 +=randomDice;
+        pp1.innerHTML = score1;
+    }
+
+    //le joueur 2 joue
+
+    if(randomDice == 1){
+        score2 = 0;
+        pp1.innerHTML = score2;
+    }
+    else
+    {
+        score2 +=randomDice;
+        pp1.innerHTML = score;2
+    }
+
     
     
+
+    
+
 }
+
+
 
 
 
